b7b3bee5cbbba98ed6eaa0fe4dcf2d2d78952de0
Eduard Munteanu <ed.munteanu99@gmail.com>
2024-04-10 20:01:11 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source "https://rubygems.org"
+
+ruby File.read(File.join(__dir__, '.ruby-version')).strip
+
+gem "rails", "~> 7.1.3", ">= 7.1.3.2"
+gem "sprockets-rails"
+gem "pg", "~> 1.1"
+gem "puma", ">= 5.0"
+gem "jsbundling-rails"
+gem "turbo-rails"
+gem "stimulus-rails"
+gem "cssbundling-rails"
+gem "jbuilder"
+gem "redis", ">= 4.0.1"
+gem "tzinfo-data", platforms: %i[ windows jruby ]
+gem "bootsnap", require: false
+gem "figaro"
+
+group :development, :test do
+  gem "debug", platforms: %i[ mri windows ]
+end
+
+group :development do
+  gem "web-console"
+end
+
+group :test do
+  gem "capybara"
+  gem "selenium-webdriver"
+end
---
+++ config/environments/test.rb
@@ -0,0 +1,44 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+  config.enable_reloading = false
+
+  config.eager_load = ENV["CI"].present?
+
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+    "Cache-Control" => "public, max-age=#{1.hour.to_i}"
+  }
+
+  config.consider_all_requests_local = true
+  config.action_controller.perform_caching = false
+  config.cache_store = :null_store
+
+  config.action_dispatch.show_exceptions = :rescuable
+
+  config.action_controller.allow_forgery_protection = false
+
+  config.active_storage.service = :test
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :test
+
+  config.active_support.deprecation = :stderr
+
+  config.active_support.disallowed_deprecation = :raise
+
+  config.active_support.disallowed_deprecation_warnings = []
+
+  config.i18n.raise_on_missing_translations = true
+  config.i18n.exception_handler = Proc.new { |exception| raise exception.to_exception }
+
+  config.action_controller.raise_on_missing_callback_actions = true
+  config.action_controller.action_on_unpermitted_parameters = :raise
+
+  config.to_prepare do
+    ActiveSupport.on_load(:active_record) do
+      ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.create_unlogged_tables = true
+    end
+  end
+end
