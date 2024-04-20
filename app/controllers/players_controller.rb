92f0bfa05d858be01ce1fa7ac7c7bb8adffa2c84
Eduard Munteanu <ed.munteanu99@gmail.com>
2024-04-20 21:28:18 +0200

    Add player index

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class PlayersController < ApplicationController
+  def index
+    # TODO: Sort by normal_rank, survivor_rank, racing_rank, defilante_rank (based on query param)
+    #   once the players are sorted by the respective scores and the ranks persisted to the DB.
+    @pagy, @players = pagy(Player.eligible_for_ranking.order(:normal_score).reverse_order)
+  end
+end
---
+++ config/initializers/pagy.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+Pagy::DEFAULT[:items] = 20
+Pagy::DEFAULT[:size] = [1, 4, 4, 1]
+
+# Bootstrap extra: Add nav, nav_js and combo_nav_js helpers and templates for Bootstrap pagination
+# See https://ddnexus.github.io/pagy/docs/extras/bootstrap
+require 'pagy/extras/bootstrap'
+
+# Overflow extra: Allow for easy handling of overflowing pages
+# See https://ddnexus.github.io/pagy/docs/extras/overflow
+require 'pagy/extras/overflow'
+Pagy::DEFAULT[:overflow] = :last_page
+
+# Trim extra: Remove the page=1 param from links
+# See https://ddnexus.github.io/pagy/docs/extras/trim
+require 'pagy/extras/trim'
+
+Pagy::DEFAULT.freeze
