b5b0180d349bac66a8712e2a4cd70661c8137b45
Eduard Munteanu <31915276+edmunteanu@users.noreply.github.com>
2024-04-23 16:05:21 +0200

    Allow specifying the batch size (#17)

--- app/services/player_updater.rb
+++ app/services/player_updater.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
 class PlayerUpdater < MultithreadedMutator
+  include Utils::PlayerMapper
+
   MIN_CHEESE_GATHERED = 1000
 
-  class << self
-    include Utils::PlayerMapper
+  private
 
-    private
+  def records
+    A801::Player.where(updatedLast7days: true, cheese_gathered: MIN_CHEESE_GATHERED..)
+  end
 
-    def records
-      A801::Player.where(updatedLast7days: true, cheese_gathered: MIN_CHEESE_GATHERED..)
-    end
-
-    def handle_record(a801_player)
-      Player.find_or_initialize_by(a801_id: a801_player.id).tap do |player|
-        player.assign_attributes(map_player(a801_player))
-        player.save! if player.new_record? || player.changed?
-      end
+  def handle_record(a801_player)
+    Player.find_or_initialize_by(a801_id: a801_player.id).tap do |player|
+      player.assign_attributes(map_player(a801_player))
+      player.save! if player.new_record? || player.changed?
     end
   end
 end
