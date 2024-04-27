9fee81df9be7671bfab1e8b3f47cce25d8f6f7d8
Eduard Munteanu <ed.munteanu99@gmail.com>
2024-04-27 22:44:37 +0200

    Add change log counter cache

--- app/models/change_log.rb
+++ app/models/change_log.rb
@@ -4,7 +4,7 @@ class ChangeLog < ApplicationRecord
   # Brand new change logs are 0 days old. To keep exactly 30 change logs, we need to expire logs older than 29 days.
   MAX_AGE = 29.days
 
-  belongs_to :player
+  belongs_to :player, counter_cache: true
 
   scope :expired, -> { where('created_at < ?', Date.current - MAX_AGE) }
 end
