bdc6799827cbdb45da47cd951d125edfb96335c3
Eduard Munteanu <ed.munteanu99@gmail.com>
2024-04-28 21:30:58 +0200

    Update rails_admin config

--- app/models/update_log.rb
+++ app/models/update_log.rb
@@ -3,4 +3,14 @@
 class UpdateLog < ApplicationRecord
   enum status: { started: 'started', updating_players: 'updating_players', updating_ranks: 'updating_ranks',
                  finished: 'finished' }
+
+  def time_lapsed
+    return if created_at.blank? || completed_at.blank?
+
+    time_lapsed = (completed_at - created_at).to_i
+    hours = time_lapsed / 1.hour
+    minutes = (time_lapsed % 1.hour) / 1.minute
+
+    "#{hours}h #{minutes}m"
+  end
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -5,7 +5,7 @@ RailsAdmin.config do |config|
   config.authenticate_with { warden.authenticate! scope: :user }
   config.current_user_method(&:current_user)
   config.main_app_name = %w[CheeseForMice Admin]
-  config.included_models = %w[User Player ChangeLog]
+  config.included_models = %w[User Player ChangeLog UpdateLog]
 
   config.actions do
     dashboard
@@ -14,7 +14,7 @@ RailsAdmin.config do |config|
   end
 
   config.model 'User' do
-    list { include_fields :id, :email, :created_at, :updated_at }
+    list { include_fields :id, :email, :role, :created_at, :updated_at }
   end
 
   config.model 'Player' do
@@ -24,12 +24,40 @@ RailsAdmin.config do |config|
 
       field(:change_logs) { pretty_value { bindings[:object].change_logs.size } }
     end
+
+    show do
+      include_all_fields
+
+      field(:created_at)
+    end
   end
 
   config.model 'ChangeLog' do
     list do
       filters %i[player created_at]
       include_fields :id, :player, :normal_score, :survivor_score, :racing_score, :defilante_score, :created_at
+    end
+
+    show do
+      field(:created_at)
+
+      include_all_fields
+    end
+  end
+
+  config.model 'UpdateLog' do
+    list do
+      filters %i[created_at]
+      include_fields :id, :status, :created_at, :completed_at
+
+      field(:time_lapsed)
+    end
+
+    show do
+      field(:status)
+      field(:created_at)
+      field(:completed_at)
+      field(:time_lapsed)
     end
   end
 end
