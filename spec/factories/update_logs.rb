aa33ce22d9732c095284ac7c62e2d6ad80c7ac17
Eduard Munteanu <ed.munteanu99@gmail.com>
2024-04-30 00:25:48 +0200

    Drop UpdateLogs

--- spec/factories/update_logs.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :update_log do
-    status { :finished }
-    created_at { Time.current }
-    completed_at { Time.current }
-  end
-end
