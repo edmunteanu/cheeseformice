c68ff3e8d6655693167c8bc8bdc8c05656f08c3e
Eduard Munteanu <31915276+edmunteanu@users.noreply.github.com>
2024-05-04 14:52:45 +0200

    Improve leaderboard page (#33)
    * Improve pagy setup and styling for the leaderboard
    
    * Improve mobile design
    
    * Linter fixes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
 class PlayersController < AuthenticatedController
+  MAX_LEADERBOARD_PAGES = 50
+
+  before_action :sanitize_params, only: :index
+
   def index
     # TODO: Sort by normal_rank, survivor_rank, racing_rank, defilante_rank (based on query param)
     #   once the players are sorted by the respective scores and the ranks persisted to the DB.
-    # TODO: Add caching after checking the leaderboard performance in production.
-    # count = Rails.cache.fetch('players_count', expires_in: 1.hour) do
-    #   Player.eligible_for_ranking.count
-    # end
-    # @pagy, @players = pagy(Player.eligible_for_ranking.order(:normal_rank), count: count)
-    @pagy, @players = pagy(Player.eligible_for_ranking.order(:normal_rank))
+    @pagy, @players = pagy_countless(Player.eligible_for_ranking.order(:normal_rank), max_pages: MAX_LEADERBOARD_PAGES)
+  end
+
+  private
+
+  # The Pagy overflow extra does not work with the :max_pages option. Setting it to :last_page in the initializer
+  # won't work with this setup, since it only allows :empty_page or :exception. Neither of these options are suitable
+  # in this context, so we overwrite the page param if it exceeds the maximum number of pages.
+  def sanitize_params
+    params[:page] = MAX_LEADERBOARD_PAGES if params[:page].to_i > MAX_LEADERBOARD_PAGES
   end
 end
