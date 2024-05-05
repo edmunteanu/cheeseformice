71799e4fd96674fb6cbc3ec6468af2a01116c6c3
Eduard Munteanu <31915276+edmunteanu@users.noreply.github.com>
2024-05-05 03:04:49 +0200

    Fix 404 format handling (#38)

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class ErrorsController < ApplicationController
+  before_action :default_format_html, only: :not_found
+
   def not_found
     render status: :not_found
   end
 
   def internal_server_error
     render status: :internal_server_error
+  end
+
+  private
+
+  def default_format_html
+    request.format = :html
   end
 end
