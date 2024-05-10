61fb2b610fda2131bdf13e2de64bf3e5d85a07ed
Eduard Munteanu <ed.munteanu99@gmail.com>
2024-05-10 16:52:44 +0200

    Implement basic structure

---
+++ app/components/player_logs.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class PlayerLogs < ViewComponent::Base
+  attr_reader :logs
+
+  def initialize(logs)
+    super
+    @logs = logs
+  end
+
+  def log_map
+    logs.index_by { |log| log.created_at.to_date }
+  end
+
+  def score_change(value)
+    return display_score(value, span_class: 'text-muted', icon_class: 'bi bi-dash-lg') if value.zero?
+    return display_score(value, span_class: 'text-success', icon_class: 'bi bi-chevron-up') if value.positive?
+
+    display_score(value.abs, span_class: 'text-danger', icon_class: 'bi bi-chevron-down')
+  end
+
+  private
+
+  def display_score(score, span_class:, icon_class:)
+    value = [
+      I18n.t('players.score', score: number_with_delimiter(score)),
+      "<i class='#{icon_class}'></i>"
+    ].join(' ')
+
+    "<span class='#{span_class}'>#{value}</span>"
+  end
+end
--- app/models/change_log.rb
+++ app/models/change_log.rb
@@ -3,14 +3,18 @@
 class ChangeLog < ApplicationRecord
   include PlayerExtensions::Ratio
 
+  MAX_AMOUNT = 30
+
   belongs_to :player, counter_cache: true
 
   # Hint: If the created_at date of the latest change log is the same as the current date, the change log contains
   # player statistics accumulated the previous day. This is because of how the updating mechanism works.
   scope :previous_day, -> { where('created_at >= ?', Date.current - 1.day).order(created_at: :desc).limit(1) }
+
+  # TODO: Might not need `previous_week` scope
   scope :previous_week, -> { where('created_at >= ?', Date.current - 1.week).order(created_at: :desc).limit(7) }
   scope(:previous_month, lambda do
-    where('created_at >= ?', Date.current - 1.month).order(created_at: :desc).limit(30)
+    where('created_at >= ?', Date.current - MAX_AMOUNT.days).order(created_at: :desc).limit(MAX_AMOUNT)
   end)
-  scope :expired, -> { where('created_at < ?', Date.current - 1.month) }
+  scope :expired, -> { where('created_at < ?', Date.current - MAX_AMOUNT.days) }
 end
