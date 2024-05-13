42cc52eef798f6e9c2cb856dedecde4ac7fd2624
Eduard Munteanu <ed.munteanu99@gmail.com>
2024-05-13 17:18:37 +0200

    Remove PlayerImporter and simplify code

--- app/services/player_updater.rb
+++ app/services/player_updater.rb
@@ -1,16 +1,38 @@
 # frozen_string_literal: true
 
-class PlayerUpdater < MultithreadedMutator
+class PlayerUpdater
   include Utils::PlayerMapper
 
-  MIN_CHEESE_GATHERED = 1000
-  MIN_BOOTCAMP_GATHERED = 250
+  def initialize(batch_size: ENV.fetch('UPDATER_BATCH_SIZE', 1000).to_i)
+    @batch_size = batch_size
+  end
+
+  MAX_POOL_SIZE = 9
+  def call
+    records.in_batches(of: @batch_size) do |batch|
+      threads = []
+      pool_size = [ActiveRecord::Base.connection_pool.size - 1, MAX_POOL_SIZE].min
+      slice_size = batch.count < pool_size ? batch.count : (batch.count / pool_size.to_f).ceil
+
+      start_threads(threads, batch, slice_size)
+
+      threads.each(&:join)
+    end
+  end
 
   private
 
+  MIN_CHEESE_GATHERED = 1000
+  MIN_BOOTCAMP_GATHERED = 250
   def records
     A801::Player.where(updatedLast7days: true, cheese_gathered: MIN_CHEESE_GATHERED..)
                 .or(A801::Player.where(updatedLast7days: true, bootcamp: MIN_BOOTCAMP_GATHERED..))
+  end
+
+  def start_threads(threads, batch, slice_size)
+    batch.each_slice(slice_size) do |slice|
+      threads << Thread.new { slice.each { |record| handle_record(record) } }
+    end
   end
 
   def handle_record(a801_player)
