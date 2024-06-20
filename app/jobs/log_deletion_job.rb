be30adb3e1acd7780037c21b1ab22b2ec2d57dd8
Eduard Munteanu <31915276+edmunteanu@users.noreply.github.com>
2024-06-20 13:13:41 +0200

    Improve DeletionLog memory usage (#70)

--- app/jobs/log_deletion_job.rb
+++ app/jobs/log_deletion_job.rb
@@ -4,6 +4,6 @@ class LogDeletionJob < ApplicationJob
   retry_on StandardError, wait: :polynomially_longer, attempts: 5
 
   def perform
-    ChangeLog.expired.destroy_all
+    ChangeLog.expired.in_batches(&:destroy_all)
   end
 end
