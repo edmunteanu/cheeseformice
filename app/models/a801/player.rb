7165434ad282e1e2864535ac1d81d55b506a7ab0
Eduard Munteanu <ed.munteanu99@gmail.com>
2025-10-07 16:06:08 +0200

    Upgrade to Rails 7.2

--- app/models/a801/player.rb
+++ app/models/a801/player.rb
@@ -4,6 +4,7 @@ module A801
   class Player < ApplicationRecord
     establish_connection(:a801) unless Rails.env.test?
 
+    self.abstract_class = true if Rails.env.test?
     self.table_name = 'player'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_14_195143) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_07_134010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
