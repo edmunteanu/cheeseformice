ecb4397780c970c5698468cfd7bdb03acb109f5e
Eduard Munteanu <ed.munteanu99@gmail.com>
2025-10-15 22:06:53 +0200

    Improve RankUpdateService

--- app/services/rank_update_service.rb
+++ app/services/rank_update_service.rb
@@ -1,60 +1,71 @@
 class RankUpdateService
-  def initialize(batch_size: 50_000)
-    @batch_size = batch_size
-  end
-
   def call
-    %w[normal survivor racing defilante].each do |mode|
-      update_rank("#{mode}_score", "#{mode}_rank", "previous_#{mode}_rank")
-    end
+    # Step 1: Precompute all ranks into a single temporary table
+    create_tmp_ranking_table
+
+    # Step 2: Apply updates in batches using the precomputed ranks
+    apply_batch_updates
+
+    # Step 3: Drop temporary table
+    ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS tmp_player_ranking;")
   end
 
   private
 
-  def update_rank(score, rank, previous_rank)
-    # Step 1: Precompute ranks into a temporary table
-    create_temp_rank_table(score, rank)
-
-    # Step 2: Apply updates in batches using the precomputed ranks
-    apply_batch_updates(rank, previous_rank)
-
-    # Step 3: Drop temporary table ahead of the next iteration
-    ActiveRecord::Base.connection.execute("DROP TABLE tmp_ranked_players;")
-  end
-
   # Order by score DESC first and then by a801_id ASC to ensure that players with the same score still
   # receive a different rank. This means that an older account will be ranked higher. This should not
   # pose a problem, since duplicate scores don't become apparent until around the 200'000th rank.
-  def create_temp_rank_table(score, rank)
-    ActiveRecord::Base.connection.execute(<<~SQL.squish)
-      CREATE TEMP TABLE tmp_ranked_players AS
+  def create_tmp_ranking_table
+    ActiveRecord::Base.connection.execute(create_tmp_ranking_table_sql)
+  end
+
+  def create_tmp_ranking_table_sql
+    <<~SQL.squish
+      CREATE TEMP TABLE tmp_player_ranking AS
       SELECT id,
-             #{rank} AS old_rank,
-             ROW_NUMBER() OVER (ORDER BY #{score} DESC, a801_id ASC) AS new_rank
+             normal_rank AS previous_normal_rank,
+             survivor_rank AS previous_survivor_rank,
+             racing_rank AS previous_racing_rank,
+             defilante_rank AS previous_defilante_rank,
+             ROW_NUMBER() OVER (ORDER BY normal_score DESC, a801_id ASC) AS new_normal_rank,
+             ROW_NUMBER() OVER (ORDER BY survivor_score DESC, a801_id ASC) AS new_survivor_rank,
+             ROW_NUMBER() OVER (ORDER BY racing_score DESC, a801_id ASC) AS new_racing_rank,
+             ROW_NUMBER() OVER (ORDER BY defilante_score DESC, a801_id ASC) AS new_defilante_rank
       FROM players
       WHERE stats_reliability != 2;
     SQL
   end
 
-  def apply_batch_updates(rank, previous_rank)
+  BATCH_SIZE = 50_000
+  def apply_batch_updates
     last_id = 0
     max_id = Player.qualified.maximum(:id) || 0
 
     while last_id < max_id
       batch_start_id = last_id + 1
-      batch_end_id = [ last_id + @batch_size, max_id ].min
+      batch_end_id = [ last_id + BATCH_SIZE, max_id ].min
 
-      ActiveRecord::Base.connection.execute(<<~SQL.squish)
-        UPDATE players
-        SET #{rank} = tmp.new_rank,
-            #{previous_rank} = tmp.old_rank
-        FROM tmp_ranked_players tmp
-        WHERE players.id = tmp.id
-          AND stats_reliability != 2
-          AND players.id BETWEEN #{batch_start_id} AND #{batch_end_id};
-      SQL
+      ActiveRecord::Base.connection.execute(apply_batch_updates_sql(batch_start_id, batch_end_id))
 
       last_id = batch_end_id
     end
+  end
+
+  def apply_batch_updates_sql(batch_start_id, batch_end_id)
+    <<~SQL.squish
+      UPDATE players
+      SET normal_rank = tmp.new_normal_rank,
+          previous_normal_rank = tmp.previous_normal_rank,
+          survivor_rank = tmp.new_survivor_rank,
+          previous_survivor_rank = tmp.previous_survivor_rank,
+          racing_rank = tmp.new_racing_rank,
+          previous_racing_rank = tmp.previous_racing_rank,
+          defilante_rank = tmp.new_defilante_rank,
+          previous_defilante_rank = tmp.previous_defilante_rank
+      FROM tmp_player_ranking tmp
+      WHERE players.id = tmp.id
+        AND stats_reliability != 2
+        AND players.id BETWEEN #{batch_start_id} AND #{batch_end_id};
+    SQL
   end
 end
