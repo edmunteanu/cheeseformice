dd304a7a8f415a4cb855b25574f60ea7e7f02862
Eduard Munteanu <ed.munteanu99@gmail.com>
2025-11-02 18:31:56 +0100

    Add sitemap, update robots.txt

---
+++ config/sitemap.rb
@@ -0,0 +1,17 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.cheeseformice.cc"
+
+SitemapGenerator::Sitemap.create do
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  add leaderboard_path
+  add search_players_path
+end
