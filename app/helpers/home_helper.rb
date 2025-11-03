9069470680ca73a7020a85265ec7aedd06823321
Eduard Munteanu <31915276+edmunteanu@users.noreply.github.com>
2025-11-03 17:44:48 +0100

    Fix span colors, add delimiter (#118)

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,15 +3,15 @@ module HomeHelper
     delta = value - previous_value
 
     return display_delta(delta, span_class: "text-muted", icon_class: "bi bi-dash-lg icon-sm") if delta.zero?
-    return display_delta(delta, span_class: "text-muted", icon_class: "bi bi-chevron-up icon-sm") if delta.positive?
+    return display_delta(delta, span_class: "text-success", icon_class: "bi bi-chevron-up icon-sm") if delta.positive?
 
-    display_delta(delta.abs, span_class: "text-muted", icon_class: "bi bi-chevron-down icon-sm")
+    display_delta(delta.abs, span_class: "text-danger", icon_class: "bi bi-chevron-down icon-sm")
   end
 
   private
 
   def display_delta(value, span_class:, icon_class:)
-    content = [ value, "<i class='#{icon_class} ms-2'></i>" ].join("")
+    content = [ number_with_delimiter(value), "<i class='#{icon_class} ms-2'></i>" ].join("")
 
     "<span class='d-flex align-items-center #{span_class} ms-3'>#{content}</span>"
   end
